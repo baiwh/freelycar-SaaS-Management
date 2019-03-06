8ac226efa4daa05e533cc423471383ea558e48e2
baiwh <471974983@qq.com>
2019-03-06 11:05:53 +0800

    登出功能+重写拦截器

--- src/main.js
+++ src/main.js
@@ -34,18 +34,33 @@ Vue.prototype.$post = post
 Vue.config.productionTip = false
 
 router.beforeEach((to, from, next) => {
-  let jwt = document.cookie
+  console.log('to.path:', to.path)
   if (to.path === '/login') {
     next()
     return
   }
-  if (jwt!=='') {
-    if (to.path === '/') {
-      next({
-        path: '/home'
-      })
+  let cookie = document.cookie
+  // 先判定cookie是否有值。是，继续。否，跳转登录页
+  if (cookie) {
+    let cookieArr = cookie.split('; ')
+    let cookieObj = {}
+    cookieArr.forEach(value => {
+      let newValue = value.split('=')
+      cookieObj[newValue[0]] = newValue[1]
+    })
+    // 判断jwt是否存在。是，继续。否，跳转到登录页
+    if (cookieObj.jwtSuperAdmin) {
+      if (to.path === '/') {
+        next({
+          path: '/home'
+        })
+      } else {
+        next()
+      }
     } else {
-      next()
+      next({
+        path: '/login'
+      })
     }
   } else {
     next({
