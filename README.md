2ab27a96595605657e97852ce06712593e5c1af5
baiwh <471974983@qq.com>
2019-04-25 17:48:18 +0800

    修改导出文件夹

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # freelycar-SaaS-Management
 freelycar-saasPC管理端
+
+
+打包后的文件在management文件夹里
--- config/index.js
+++ config/index.js
@@ -48,10 +48,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../management/index.html'),
+    // index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../management'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/management/',
 
