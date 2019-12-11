fa9d86c9d245a31ef0e3e650acaab1e54f7566e0
baiwh <471974983@qq.com>
2019-12-11 16:43:03 +0800

    活动统计

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,13 +1,91 @@
 <template>
   <div>
-    <img class="page-center" src="./../../static/logo.jpg" alt="logo">
-    <div class="page-center">欢迎使用小易爱车门店管理系统</div>
+    <!--<img class="page-center" src="./../../static/logo.jpg" alt="logo">-->
+    <div class="page-center">数据统计</div>
+    <el-row :gutter="5">
+
+      <el-col :span="5">
+        <span>所属门店：</span>
+        <el-select clearable v-model="selectStore"
+                   @change="getList"
+                   placeholder="请选择" style="width: 10vw" size="small">
+          <el-option v-for="item in storeList" :key="item.id"
+                     :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-col>
+
+      <el-col :span="7">
+        <span>查找日期：</span>
+        <el-date-picker
+          v-model="datePickerValue"
+          type="date"
+          value-format="yyyy-MM-dd"
+          size="small"
+          placeholder="选择日期">
+        </el-date-picker>
+
+      </el-col>
+      <el-col :span="5">
+        <el-button type="primary" @click="getList" size="small">查询</el-button>
+      </el-col>
+
+    </el-row>
+
+    <el-table v-loading="loading" :data="dataList">
+      <el-table-column property="cumulateUserCount"
+                       :label="dataList[0].yesterdayFlag?'昨日关注人数':'当日关注人数'" align="center"/>
+      <el-table-column property="registerUserCount" label="注册人数" align="center"/>
+      <el-table-column property="orderCount" label="订单数" align="center"/>
+    </el-table>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'home'
+    name: 'home',
+    data(){
+      return{
+        selectStore:'',
+        storeList:[],
+        datePickerValue:'',
+        dataList:[],
+        loading:true
+      }
+    },
+    methods:{
+      // 获取列表
+      getList(){
+        this.loading=true
+        this.$get('/wxUserInfo/getCumulateThree', {
+          storeId: this.selectStore,
+          refDate: this.datePickerValue
+        }).then(res => {
+          this.loading=false
+          let list = [res]
+          this.dataList = list
+        })
+      },
+
+      // 获取门店列表
+      getStoreList() {
+        this.$get('/store/list', {
+          name: '',
+          currentPage: 1,
+          pageSize: 100
+        }).then(res => {
+          this.storeList = res.data
+        })
+      },
+    },
+    mounted: function () {
+      let date = new Date()
+      let year = date.getFullYear()
+      let month = date.getMonth() < 9 ? '-0' + (date.getMonth() + 1) : '-' + (date.getMonth() + 1)
+      let day = date.getDate() < 10 ? '-0' + date.getDate() : '-' + date.getDate()
+      this.datePickerValue = year + month + day
+      this.getList()
+      this.getStoreList()
+    }
   }
 </script>
 <style lang="less" scoped>
