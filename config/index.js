d9944c810e31df1cf27c66f72959aad1fb0d4c31
baiye0 <1159428886@qq.com>
2020-06-16 13:47:56 +0800

    logo修改

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     // Various Dev Server settings
     // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '192.168.0.118', // can be overwritten by process.env.HOST
+    host: '192.168.0.161', // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
